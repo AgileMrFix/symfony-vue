a7b8b07ed1226d8aad4ecf0e966b0d51a61a24b4
DH <agile.mr.fix@gmail.com>
2023-01-11 20:38:09 -0800

    api controller

---
+++ app/src/Controller/CustomerController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Address;
+use App\Entity\Customer;
+use App\Repository\CustomerRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/customer')]
+class CustomerController extends AbstractController
+{
+    #[Route('/', name: 'api_customer_index', methods: ['GET'])]
+    public function index(CustomerRepository $customerRepository, Request $request): Response
+    {
+        //TODO validations
+        $search = $request->get('s');
+
+        // orderBy[first_name]=asc, orderBy[last_name]=desc
+        $orderBy = $request->get('o',[]);
+
+        // f[country.name]=Canada
+        $filter = $request->get('f',[]);
+
+        $customers = $customerRepository->findByAllColumns($search, $orderBy, $filter);
+        //TODO make resource class
+        $data = [];
+        foreach ($customers as $customer)
+            /** @var Customer $customer */
+            $data[] = [
+                'id' => $customer->getId(),
+                'first_name'=> $customer->getFirstName(),
+                'last_name'=> $customer->getLastName(),
+                'email' => $customer->getEmail(),
+                'address' => $this->getAddressString($customer->getAddress()),
+            ];
+
+        return $this->json($data);
+    }
+
+    //TODO make service
+    private function getAddressString(Address|null $address): string
+    {
+        if (!$address)
+            return '';
+
+        $arr = [
+            $address->getLine2(),
+            $address->getLine1(),
+            $address->getState()->getName(),
+            $address->getCountry()->getName()
+        ];
+
+        //filter empty columns
+        $arr = array_filter($arr, fn($i) => $i);
+
+        return implode(', ', $arr);
+    }
+}
--- app/src/Repository/CustomerRepository.php
+++ app/src/Repository/CustomerRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Customer;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -39,20 +40,37 @@ class CustomerRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Customer[] Returns an array of Customer objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @param $value
+     * @return QueryBuilder Returns an array of Customer objects
+     */
+    public function findByAllColumns($search = null, $orders = []): array
+    {
+        $q = $this->createQueryBuilder('c')
+            ->leftJoin('c.address', 'address')
+            ->leftJoin('address.country', 'country')
+            ->leftJoin('address.state', 'state');
+
+
+        if ($search)
+            $q->andWhere('c.email LIKE :val')
+                ->orWhere('c.first_name LIKE :val')
+                ->orWhere('c.last_name LIKE :val')
+                ->orWhere('address.line_1 LIKE :val')
+                ->orWhere('address.postal_code LIKE :val')
+                ->orWhere('country.name LIKE :val')
+                ->orWhere('state.name LIKE :val')
+                ->setParameter('val', '%' . $search . '%');
+
+        foreach ($orders as $column => $order)
+            $q->orderBy($column, $order);
+
+
+        return $q->getQuery()
+            ->getResult();
+    }
+
+
 
 //    public function findOneBySomeField($value): ?Customer
 //    {
